88f209c3abab41fd2a9360e52bd8c82cb6ee417d
ahammednibras8 <ahammednibras@proton.me>
2025-01-08 00:42:15 +0530

    Set Up Backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(cors());
+
 app.get("/", (req, res) => {
     res.send("Backend is working");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port http://localhost:${PORT}`);
 })
