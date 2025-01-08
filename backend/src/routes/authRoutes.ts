09c1c441872f66db14815ddb86a555ef119ac0f4
ahammednibras8 <ahammednibras@proton.me>
2025-01-08 14:12:02 +0530

    Add roles (e.g., admin, user) to your database schema

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -2,14 +2,13 @@ import express, { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from 'jsonwebtoken';
 import dotenv from "dotenv";
+import prisma from "../config/db";
 import { verifyToken } from "../middleware/authMiddleware";
+import { error } from "console";
 
-const router = express.Router();
 dotenv.config();
 
-//Test Data (TODO: Update with real Database):
-const users: { id: number; username: string; password: string; role: string }[] = [];
-let userId = 1;
+const router = express.Router();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET) {
@@ -17,52 +16,64 @@ if (!JWT_SECRET) {
 }
 
 // User Registration:
-router.post('/register', async (req: Request, res: Response): Promise<any> => {
+router.post('/register', async (req: Request, res: Response): Promise<void> => {
     const { username, password, role } = req.body;
 
     if (!username || !password) {
-        return res.status(400).json({ message: "Username and password are required" });
+        res.status(400).json({ message: "Username and password are required" });
+        return;
+    }
+
+    //Check Existing User
+    const existingUser = await prisma.user.findUnique({
+        where: { username },
+    });
+    if (existingUser) {
+        res.status(409).json({ message: "Username already exists" });
+        return;
     }
 
     // Role Check
     if (!role || !["user", "admin"].includes(role)) {
-        return res.status(400).json({ message: "Invalid role. Allowed roles: user, admin." });
+        res.status(400).json({ message: "Invalid role. Allowed roles: user, admin." });
+        return;
 
-    }
-
-    //Username already exist:
-    const existingUser = users.find((user) => user.username === username);
-    if (existingUser) {
-        return res.status(400).json({ message: "Username already exist" });
     }
 
     //Password Hash
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = { id: userId++, username, password: hashedPassword, role };
-    users.push(newUser);
-
-    res.status(201).json({ message: 'User registered successfully', userId: newUser.id });
+    try {
+        const user = await prisma.user.create({
+            data: { username, password: hashedPassword, role },
+        });
+        res.status(201).json({ message: "User registration successfully", user });
+    } catch (e) {
+        res.status(500).json({ error: "Database error" });
+    }
 });
 
 //User Login:
-router.post('/login', async (req: Request, res: Response): Promise<any> => {
+router.post('/login', async (req: Request, res: Response): Promise<void> => {
     const { username, password } = req.body;
 
     if (!username || !password) {
-        return res.status(400).json({ message: "Username and password are required" });
+        res.status(400).json({ message: "Username and password are required" });
+        return;
     }
 
     // Find User
-    const user = users.find((user) => user.username === username);
+    const user = await prisma.user.findUnique({ where: { username } });
     if (!user) {
-        return res.status(404).json({ message: "User not found" });
+        res.status(404).json({ message: "User not found" });
+        return;
     }
 
     //Verify the password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-        return res.status(401).json({ message: "Invalid credentials" });
+        res.status(401).json({ message: "Invalid credentials" });
+        return;
     }
 
     //Generate JWT TOKEN
